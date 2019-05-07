cef24405b72a992cab6ae057b17a42a012924496
Trey Schneider <trey@codeschneider.com>
2019-05-07 12:34:32 -0400

    add socket_reducer to root_reducer

--- client/src/redux/reducers/root_reducer.js
+++ client/src/redux/reducers/root_reducer.js
@@ -2,10 +2,12 @@ import { combineReducers } from "redux";
 import { connectRouter } from "connected-react-router";
 import { reducer as oidcReducer } from "redux-oidc";
 import { reducer as appReducer } from "./app";
+import { socketReducer } from './socket_reducer'
 
 export default history =>
     combineReducers({
         router: connectRouter(history),
         oidc: oidcReducer,
-        app: appReducer
+        app: appReducer,
+        socket: socketReducer
     });
