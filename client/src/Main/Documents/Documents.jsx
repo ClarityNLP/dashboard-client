40fced29bc64e9554c706a9e122e7a200f865e42
Trey Schneider <trey@codeschneider.com>
2019-05-07 11:48:19 -0400

    rework to include react-svg-piechart, apply correct comparator in componentDidUpdate method

--- client/src/Main/Documents/Documents.jsx
+++ client/src/Main/Documents/Documents.jsx
@@ -1,81 +1,124 @@
 import React, { Component } from "react";
 import Card from "../Card";
-import PieChart from "./PieChart";
+import PieChart from "react-svg-piechart"
 import colors from "./Colors";
+import isEqual from 'lodash.isequal';
 
 export default class Documents extends Component {
     constructor(props) {
-        super(props);
-
-        this.state = {
-            content: []
-        };
+      super(props);
+      this.state = {
+        data: [],
+        isError: false,
+        message: null
+      };
     }
 
     componentDidMount() {
-        this.setContent();
+      this.setContent();
     }
 
     componentDidUpdate(prevProps) {
-        if (prevProps.app.documents !== this.props.app.documents) {
-            this.setContent();
-        }
+      if (!isEqual(prevProps.documents, this.props.documents)) {
+        this.setContent();
+      }
     }
 
     setContent = () => {
-        const { documents } = this.props.app;
-        let tmp_content;
+      const { documents } = this.props;
+      const __this = this;
 
-        if (Array.isArray(documents)) {
-            if (documents.length > 0) {
-                const data = [];
-                let color_count = 0;
+      if (Array.isArray(documents)) {
+        if (documents.length > 0) {
+          const data = [];
+          let color_count = 0;
 
-                for (let i = 0; i < documents.length; i += 2) {
-                    let label = documents[i];
-                    let count = documents[i + 1];
+          for (let i = 0; i < documents.length; i += 2) {
+            let title = documents[i];
+            let count = documents[i + 1];
 
-                    data.push({
-                        label: label,
-                        value: count,
-                        color: colors[color_count]
-                    });
+            data.push({
+              title: title,
+              value: count,
+              color: colors[color_count]
+            });
 
-                    color_count++;
-                }
+            color_count++;
+          }
 
-                tmp_content = <PieChart data={data} radius={100} />;
-            } else {
-                tmp_content = <p>You have no documents available.</p>;
-            }
+          __this.setState({
+            data: data,
+            isError: false,
+            message: null
+          });
         } else {
-            tmp_content = (
-                <p className="has-text-weight-bold">
-                    We ran into a problem while getting your documents, please
-                    try again later.
-                </p>
-            );
+          __this.setState({
+            data: [],
+            isError: false,
+            message: 'You have no documents available.'
+          });
         }
+      } else {
+        __this.setState({
+          data: [],
+          isError: true,
+          message: 'We ran into a problem while getting your documents, please try again later.'
+        });
+      }
+    };
 
-        this.setState({
-            content: tmp_content
+    getLegend = (data) => {
+        return data.map((key, i) => {
+            const { value, color, title } = key;
+            return (
+                <tr key={"legend" + i}>
+                    <td style={{ backgroundColor: color }} />
+                    <td>{title}</td>
+                    <td>{value}</td>
+                </tr>
+            );
         });
     };
 
     render() {
-        const { content } = this.state;
+      const {
+        data,
+        isError,
+        message
+      } = this.state;
 
-        return (
-            <Card
-                className="documents"
-                heading="Documents"
-                cta_label="Add Documents"
-                cta_href={
-                    "http://" + window._env_.REACT_APP_INGEST_URL + "/csv"
-                }
-            >
-                <div className="document_pie_chart">{content}</div>
-            </Card>
-        );
+      let content;
+
+      if (data.length > 0 && !isError) {
+        content = <PieChart data={data}/>;
+      } else if (data.length === 0 && !isError) {
+        content = <p>{message}</p>;
+      } else {
+        content = <p className="has-text-weight-bold">{message}</p>;
+      }
+
+      return (
+        <Card
+          className="documents"
+          heading="Documents"
+          cta_label="Add Documents"
+          cta_href={"http://" + window._env_.REACT_APP_INGEST_URL + "/csv"}
+        >
+          <div className="document_pie_chart">
+            <div className="columns card_columns">
+              <div className="column is-half is-narrow">
+                {content}
+              </div>
+              <div className="column is-half is-narrow">
+                <div className="pie_legend">
+                  <table className="table is-narrow is-fullwidth">
+                    <tbody>{this.getLegend(data)}</tbody>
+                    </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Card>
+      );
     }
 }
