4a1a5199cfef65771115ad55df474681511bc27a
Trey Schneider <trey@codeschneider.com>
2019-09-04 13:00:57 -0400

    public_url as build arg adjustments

--- client/src/index.js
+++ client/src/index.js
@@ -12,7 +12,7 @@ import * as serviceWorker from './serviceWorker';
 // serviceWorker.register(); //TODO causing issue on USER_EXPIRED
 
 const history = createBrowserHistory({
-  basename: window._env_.PUBLIC_URL
+  basename: process.env.PUBLIC_URL
 });
 const initialState = {};
 const socketClient = new SocketClient();
